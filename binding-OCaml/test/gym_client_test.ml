9fe0295bf1da221700db3e5484100ee6b8c00662
panyuewh <worm_hole5565@hotmail.com>
2024-02-10 00:37:23 +0800

    try to make fmt pass

--- binding-OCaml/test/gym_client_test.ml
+++ binding-OCaml/test/gym_client_test.ml
@@ -3,7 +3,7 @@ open OUnit2
 
 let tests = "test suite" >::: [
   "empty" >:: (fun _ -> assert_equal 0 0);
-  "singleton" >:: (fun _ -> assert_equal 1 0);
+  "singleton" >:: (fun _ -> assert_equal 1 1);
 ]
 
 let _ = run_test_tt_main tests
